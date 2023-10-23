a9e8ec4da418aa08b3de5021b5bc772378ea29f0
Victor Faure <faurevic@gmail.com>
2023-10-23 11:35:07 +0200

    Added back all files...

---
+++ render.js
@@ -0,0 +1,18 @@
+function renderme(prefix = '') {
+    fetch(prefix + 'templates/header.html')
+    .then((response) => response.text())
+    .then((template) => {
+        const rendered = Mustache.render(template, {
+            home: 'Home'
+        });
+        document.getElementById('header-target').innerHTML = rendered;
+    })
+    fetch(prefix + 'templates/footer.html')
+    .then((response) => response.text())
+    .then((template) => {
+        const rendered = Mustache.render(template, {
+            github: 'https://github.com/N1ghtress'
+        });
+        document.getElementById('footer-target').innerHTML = rendered;
+    })
+}
